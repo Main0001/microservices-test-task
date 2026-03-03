b7bb6bf32233382f6c40df84c2ef235944686bb1
Maxim Mushinskiy <makmushinskiy@inbox.ru>
2026-03-03 20:41:11 +0300

    configured the main module

--- invoice-service/src/app.module.ts
+++ invoice-service/src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullModule } from '@nestjs/bullmq';
+import appConfig from './config/app.config';
+import mailConfig from './config/mail.config';
+import redisConfig from './config/redis.config';
+import { PrismaModule } from './prisma/prisma.module';
+// import { ClientModule } from './modules/client/client.module';
+// import { InvoiceModule } from './modules/invoice/invoice.module';
+// import { PdfModule } from './modules/pdf/pdf.module';
+// import { MailModule } from './modules/mail/mail.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, mailConfig, redisConfig],
+    }),
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        connection: {
+          host: config.get<string>('redis.host'),
+          port: config.get<number>('redis.port'),
+        },
+      }),
+    }),
+    PrismaModule,
+    // ClientModule,
+    // InvoiceModule,
+    // PdfModule,
+    // MailModule,
+  ],
 })
 export class AppModule {}
--- invoice-service/src/main.ts
+++ invoice-service/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Invoice Service')
+    .setDescription('API для генерации и отправки инвойсов')
+    .setVersion('1.0')
+    .build();
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('app.port')!;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
